ab69aa8821c23692c9fbfbc4959829d345970220
Mahad Sarfraz <mahadsarfraz13@gmail.com>
2023-02-11 00:53:11 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+// Routes
+
+app.use("/api/v1/products", productsRouter);
+
+// Products route middleware
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is running on port ${port} .. `));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
